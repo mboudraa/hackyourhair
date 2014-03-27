8f41afeefeeebb4f169428b64f14df4ed1a7d2f0
Gautier Mechling <gmechling@xebia.fr>
2014-03-27 15:15:28 +0100

    Add haircuts

--- HackYourHair/src/main/java/fr/bemyapp/hackyourhair/ResultActivity.java
+++ HackYourHair/src/main/java/fr/bemyapp/hackyourhair/ResultActivity.java
@@ -1,6 +1,9 @@
 package fr.bemyapp.hackyourhair;
 
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
@@ -12,6 +15,18 @@ public class ResultActivity extends BaseActivity {
     private static final int MENU_HAIRCUTS = 1;
     private static final int MENU_ADVICES = 2;
 
+    @ViewById(R.id.result_component_products)
+    View mCompProducts;
+
+    @ViewById(R.id.result_component_haircuts)
+    View mCompHaircuts;
+
+    @ViewById(R.id.result_component_advices)
+    View mCompAdvices;
+
+    @ViewById(R.id.result_subcontainer)
+    FrameLayout mContainer;
+
     @ViewById(R.id.result_products)
     TextView mMenuProducts;
 
@@ -21,19 +36,27 @@ public class ResultActivity extends BaseActivity {
     @ViewById(R.id.result_advices)
     TextView mMenuAdvices;
 
+    @AfterViews
+    void afterViews() {
+        onProductsClicked();
+    }
+
     @Click(R.id.result_advices)
     void onAdvicesClicked() {
         updateHeadlinesColors(MENU_ADVICES);
+        changeSubView(MENU_ADVICES);
     }
 
     @Click(R.id.result_haircuts)
     void onHaircutsClicked() {
         updateHeadlinesColors(MENU_HAIRCUTS);
+        changeSubView(MENU_HAIRCUTS);
     }
 
     @Click(R.id.result_products)
     void onProductsClicked() {
         updateHeadlinesColors(MENU_PRODUCTS);
+        changeSubView(MENU_PRODUCTS);
     }
 
     void updateHeadlinesColors(int type) {
@@ -53,6 +76,24 @@ public class ResultActivity extends BaseActivity {
                 break;
             case MENU_ADVICES:
                 mMenuAdvices.setTextColor(selectedColor);
+                break;
+        }
+    }
+
+    void changeSubView(int type) {
+        mCompAdvices.setVisibility(View.GONE);
+        mCompHaircuts.setVisibility(View.GONE);
+        mCompProducts.setVisibility(View.GONE);
+
+        switch (type) {
+            case MENU_PRODUCTS:
+                mCompAdvices.setVisibility(View.VISIBLE);
+                break;
+            case MENU_HAIRCUTS:
+                mCompHaircuts.setVisibility(View.VISIBLE);
+                break;
+            case MENU_ADVICES:
+                mCompProducts.setVisibility(View.VISIBLE);
                 break;
         }
     }
